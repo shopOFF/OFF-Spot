1988be91c3fb5ca67647494e6d9ed034cc03dfc0
shopoff <shopov91@gmail.com>
2018-01-21 22:36:52 +0200

    renamed projects

---
+++ OFF-Spot/OFF-Spot.Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpotOFF.Models
+{
+    public class User
+    {
+    }
+}
--- OFF-Spot/SpotOFF.Web/Startup.cs
+++ OFF-Spot/SpotOFF.Web/Startup.cs
@@ -8,9 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using OFF_Spot.Web.Data;
-using OFF_Spot.Web.Models;
-using OFF_Spot.Web.Services;
+using SpotOFF.Web.Data;
+using SpotOFF.Web.Models;
+using SpotOFF.Web.Services;
 
 namespace SpotOFF.Web
 {
